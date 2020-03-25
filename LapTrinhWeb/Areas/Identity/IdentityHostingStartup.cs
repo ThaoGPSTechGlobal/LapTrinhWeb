da92742c2f88835958f044e70982a8f988923b38
=ThaoGPSTechGlobal <=it.thaont@gmail.com>
2020-03-25 22:58:16 +0700

    Add project files.

---
+++ LapTrinhWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LapTrinhWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LapTrinhWeb.Areas.Identity.IdentityHostingStartup))]
+namespace LapTrinhWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
